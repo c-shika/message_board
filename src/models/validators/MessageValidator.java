b9679c11c044be5b9c1df1baefe4e88af10689ce
takeshi.shikanai <yoisyo61@gmail.com>
2021-07-19 02:06:48 +0900

    バリデーションを追加

---
+++ src/models/validators/MessageValidator.java
@@ -0,0 +1,44 @@
+package models.validators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Message;
+
+public class MessageValidator {
+    //
+    public static List<String> validate(Message m){
+        //バリデージョンを実行する
+        List<String> errors = new ArrayList<String>();
+
+        String title_error = validateTitle(m.getTitle());
+        if(!title_error.equals("")) {
+            errors.add(title_error);
+        }
+
+        String content_error = validateContent(m.getContent());
+        if(!content_error.equals("")) {
+            errors.add(content_error);
+        }
+
+        return errors;
+    }
+
+    //必須入力チェック
+    private static String validateTitle(String title) {
+        if(title == null || title.equals("")) {
+            return "タイトルを入力してください。";
+        }
+
+        return "";
+    }
+
+    //必須入力チェック
+    private static String validateContent(String content) {
+        if(content == null || content.equals("")) {
+            return "メッセージを入力してください。";
+        }
+
+        return "";
+    }
+}
